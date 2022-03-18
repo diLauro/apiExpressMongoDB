84338f6f541d9f5ba9c904708af51c0a55d81231
Eduardo Di Lauro <eduardo.dilauro@gmail.com>
2022-03-18 11:17:46 -0300

    feat: modulo 2

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+const app = express();
+
+const livros = [
+  { id: 1, titulo: "Senhor dos Aneis" },
+  { id: 2, titulo: "O Hobiit" },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso.");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const { id } = req.params;
+  const index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso.`);
+});
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
+export default app;
